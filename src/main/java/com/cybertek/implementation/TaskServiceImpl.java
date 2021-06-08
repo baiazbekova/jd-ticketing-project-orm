f00c43e6d6051bacfec801f7362739ec1a1954b4
baiazbekova <baiazbekova@gmail.com>
2021-06-08 12:25:49 -0400

    updates

--- src/main/java/com/cybertek/implementation/TaskServiceImpl.java
+++ src/main/java/com/cybertek/implementation/TaskServiceImpl.java
@@ -1,13 +1,31 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.TaskDTO;
+import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.Task;
+import com.cybertek.enums.Status;
+import com.cybertek.mapper.MapperUtil;
+import com.cybertek.mapper.TaskMapper;
+import com.cybertek.repository.TaskRepository;
 import com.cybertek.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    TaskRepository taskRepository;
+    TaskMapper taskMapper;
+
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
     @Override
     public TaskDTO findById(Long id) {
         return null;
@@ -15,12 +33,17 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        List<Task> list = taskRepository.findAll();
+        return list.stream().map(taskMapper:: convertToDTO).collect(Collectors.toList());
+
     }
 
     @Override
     public Task save(TaskDTO dto) {
-        return null;
+       dto.setTaskStatus(Status.OPEN);
+       dto.setAssignedDate(LocalDate.now());
+       Task task = taskMapper.convertToEntity(dto);
+       return taskRepository.save(task);
     }
 
     @Override
---
+++ src/main/java/com/cybertek/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+
+
+}
